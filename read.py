d323fbdf4411c344d2026f349e931ebeeb4929a6
BE3dARt <lernwerk@7CGR45J.intranet.lernwerk.ch>
2022-04-11 17:01:26 +0200

    Transfer to other workstation

--- read.py
+++ read.py
@@ -41,30 +41,42 @@ for x in range(len(content)):
                 tempStringColors = ""
 
     #Capture configuration window
-    if mainCounter == 3 and levelCounter == 2:
+    if mainCounter >= 2 and levelCounter == 2:
         if content[x] != " " and content[x] != "\n":
             
             #Line does always end with ",". Proceed to format and store this line 
             if content[x] == ",":
                 
-                
                 #Temporary variables until passed to top array
-                tempContents = [""]
+                tempContents = []
                 tempContentsString = ""
                 
                 #Dependent on which type is wanted, format the string differently
                 if windowContentsOneLine[0] == "Two-Side" or windowContentsOneLine[0] == "One-Side" or windowContentsOneLine[0] == "Display" or windowContentsOneLine[0] == "Menu" or windowContentsOneLine[0] == "Menu-Display":
-                    for i in range(len(windowContentsOneLine[1]))
+                    for i in range(len(windowContentsOneLine[1])):
+
+                        #If there is a seperator (","), split the string and append it to the array
                         if windowContentsOneLine[1][i] == ",":
                             tempContents.append(tempContentsString)
                             tempContentsString = ""
                         else:
-                            tempContentsString += windowContentsOneLine[1][i]
+                            #Delete the space after a seperator (","), eg. "Test, ABC" -> "Test,ABC"
+                            if windowContentsOneLine[1][i-1] != ",":
+                                tempContentsString += windowContentsOneLine[1][i]
+                            #Append to array if line is finished
+                            if i == len(windowContentsOneLine[1])-1:
+                                tempContents.append(tempContentsString)
                             
                 if windowContentsOneLine[0] == "Configuration":
-                    print("Not Ready! LOL")
-                
-                if tempContents == [""]:
+                    fakeLevelCounter = 0
+                    for i in range(len(windowContentsOneLine[1])):
+
+                        #Activate if a row has ended
+                        if windowContentsOneLine[1][i] == "\n" and (windowContentsOneLine[1][i-1] == "," or windowContentsOneLine[1][i-1] == "]"):
+                            print (windowContentsOneLine[1])
+                            print ("YESYES")
+
+                if tempContents == []:
                     windowContents.append(windowContentsOneLine[0])
                 else:
                     windowContents.append([windowContentsOneLine[0], tempContents])
@@ -75,9 +87,9 @@ for x in range(len(content)):
             else:
                 windowContentsOneLine[0] += content[x]
             
-    if mainCounter == 3 and levelCounter >= 3:
+    if mainCounter >= 2 and levelCounter >= 3:
         windowContentsOneLine[1] += content[x]
 
 
-#print (windowContents)
+print (windowContents[1])
 input()
