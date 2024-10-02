0423692f1642b5391ac34fe7989577c9ae7c0e57
Dreamfarer <dreamfarer@pm.me>
2024-10-02 10:36:20 +0200

    doc: bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="clibb",
-    version="0.2.1",
+    version="0.2.2",
     description="CLIBB (Command-Line Interface Building Blocks) streamlines, simplifies and speeds up your CLI creation.",
     long_description=long_description,
     long_description_content_type="text/markdown",
